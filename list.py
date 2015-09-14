e973e355391a7e85a55cba64d2ed768d7a9e5992
Justin Foley <justinrfoley@yahoo.com>
2015-09-14 19:42:58 +0000

    Fixed issue where course sections with letters would not work. Added ability to put crn or course number in program

--- list.py
+++ list.py
@@ -2,16 +2,27 @@ import sys
 import re
 import csv
 
-fullCourse=sys.argv[1].translate(None,'[]').split('.')
-secNum=fullCourse[1]
-course=re.match(r"([A-Z]+)([0-9]+)",fullCourse[0],re.I)
-if course:
-	items=course.groups()
-f=open(sys.argv[2].translate(None,'[]'),'rb')
-try:
-	reader=csv.DictReader(f)
-	for row in reader:
-		if row['Subject Code']==items[0] and row['Course Number']==items[1] and int(row['Section Number'])==int(secNum):
-			print row['First Name']+' '+row['Last Name']
-finally:
-	f.close()
+if re.search('[a-zA-Z]',sys.argv[1]):
+	fullCourse=sys.argv[1].translate(None,'[]').split('.')
+	secNum=fullCourse[1]
+	course=re.match(r"([A-Z]+)([0-9]+)",fullCourse[0],re.I)
+	if course:
+		items=course.groups()
+	f=open(sys.argv[2].translate(None,'[]'),'rb')
+	try:
+		reader=csv.DictReader(f)
+		for row in reader:
+			if row['Subject Code']==items[0] and row['Course Number']==items[1] and ((re.search('[a-zA-Z]', row['Section Number']) and re.search('[a-zA-Z]', secNum) and row['Section Number']==secNum) or (not re.search('[a-zA-Z]', row['Section Number']) and not re.search('[a-zA-Z]', secNum) and int(row['Section Number'])==int(secNum))):
+				print row['First Name']+' '+row['Last Name']
+	finally:
+		f.close()
+else:
+	crn=sys.argv[1]
+	f=open(sys.argv[2].translate(None,'[]'),'rb')
+	try:
+		reader=csv.DictReader(f)
+		for row in reader:
+			if row['CRN']==crn:
+				print row['First Name']+' '+row['Last Name']
+	finally:
+		f.close()
