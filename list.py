7a120143bbe5da541fcf4b955d232e4a1caf0ca4
Ethan Rutherford <ethanrutherford2@yahoo.com>
2015-09-18 15:19:41 -0500

    select from new tables

--- list.py
+++ list.py
@@ -1,53 +1,32 @@
 import sys
 import re
-import csv
 import mysql.connector
 
 db = mysql.connector.connect(user='root', password='password',
 	host='127.0.0.1', database='db')
 cursor = db.cursor()
 
-query = ""
+crn = ""
 
-if re.search('[a-zA-Z]',sys.argv[1]):
-	fullCourse=sys.argv[1].translate(None,'[],').split('.')
-	secNum=fullCourse[1]
-	course=re.match(r"([A-Z]+)([0-9]+)",fullCourse[0],re.I)
+if re.search('[a-zA-Z]', sys.argv[1]):
+	fullCourse = sys.argv[1].translate(None, '[],').split('.')
+	secNum = fullCourse[1]
+	course = re.match(r"([A-Z]+)([0-9]+)", fullCourse[0], re.I)
 	if course:
-		items=course.groups()
-	query = ''.join(["select `First Name`, `Last Name` from registrations where",
-		"`Subject Code` = '", items[0], "' and `Course Number` = ", items[1],
-		" and `Section Number` like '%", secNum, "%';"])
-	# try:
-		# f=open(sys.argv[2].translate(None,'[]'),'rb')
-	# except IndexError:
-		# f=open("CS374_2016_registrations.csv",'rb')
-	# try:
-		# reader=csv.DictReader(f)
-		# for row in reader:
-			# if row['Subject Code']==items[0] and row['Course Number']==items[1] and ((re.search('[a-zA-Z]', row['Section Number']) and re.search('[a-zA-Z]', secNum) and row['Section Number']==secNum) or (not re.search('[a-zA-Z]', row['Section Number']) and not re.search('[a-zA-Z]', secNum) and int(row['Section Number'])==int(secNum))):
-				# print row['First Name']+' '+row['Last Name']
-	# finally:
-		# f.close()
+		items = course.groups()
+	cursor.execute(''.join(["select `CRN` from ",
+		"section where `Subject Code` = '", items[0], 
+		"' and `Course Number` = ", items[1], " and `Section Number` like '%", 
+		secNum, "';"])
+	crn = cursor.fetchone()[0];
 else:
-	crn=sys.argv[1].translate(None,'[],')
-	query = ''.join(["select `First Name`, `Last Name` from registrations where ",
-		"CRN = '", crn, "';"])
-	# try:
-		# f=open(sys.argv[2].translate(None,'[]'),'rb')
-	# except IndexError:
-		# f=open("CS374_2016_registrations.csv",'rb')
-	# try:
-		# reader=csv.DictReader(f)
-		# for row in reader:
-			# if row['CRN']==crn:
-				# print row['First Name']+' '+row['Last Name']
-	# finally:
-		# f.close()
+	crn = sys.argv[1].translate(None,'[],')
 
-cursor.execute(query)
+cursor.execute(''.join(["select s.`First Name`, s.`Last Name`, s.`BannerID` from",
+	" enrollment e inner join student s on e.`Banner Id` = s.`Banner ID` where ",
+	"CRN = '", crn, "';"]))
 
-for (fname, lname) in cursor:
-	print fname + ' ' + lname
+for (fname, lname, banner) in cursor:
+	print fname + ' ' + lname + ', ' + banner
 
 db.close();
