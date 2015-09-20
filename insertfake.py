40105f612229ba32a29a13a57f4c3710bd336dc3
Ethan Rutherford <ethanrutherford2@yahoo.com>
2015-09-20 15:49:55 -0500

    one more time

--- insertfake.py
+++ insertfake.py
@@ -23,5 +23,5 @@ for (banner) in banners:
 		year = random.randint(2010, 2015)
 		if (year == 2015 and sem == 3):
 			sem = 2
-		cursor.execute(''.join(["insert into enrollment values('", str(banner), "','",
-			str(course), "','", str(sem), "','", str(year), "','", str(grade), "');"]))
+		cursor.execute(''.join(["insert into enrollment values(", str(banner), ",",
+			str(course), ",", str(sem), ",", str(year), ",'", str(grade), "');"]))
