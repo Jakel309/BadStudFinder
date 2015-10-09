e0b9e10db0482545a43f02f2d1b18371bde1e8c8
Justin Foley <justinrfoley@yahoo.com>
2015-10-09 18:59:12 +0000

    Added external connection for import script

--- import.py
+++ import.py
@@ -1,8 +1,25 @@
 import mysql.connector
 from glob import glob
+import ConfigParser
 
-db = mysql.connector.connect(user='root', password='password',
-	host='127.0.0.1', database='db', allow_local_infile=True)
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+Config.sections()
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+db = mysql.connector.connect(user=ConfigSectionMap("Database")['user'], password=ConfigSectionMap("Database")['password'],
+	host=ConfigSectionMap("Database")['host'], database=ConfigSectionMap("Database")['database'])
 cursor = db.cursor()
 
 #create (replace) registration table
